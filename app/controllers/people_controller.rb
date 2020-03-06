e0dd0f7863a19a09c036f0946f3dd3721a8795c8
Ernesto <e.ernesto.espinosa@gmail.com>
2020-03-06 04:01:40 -0600

    [3] Create a function that transforms and groups the emails into a char frequency map

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -16,9 +16,27 @@ class PeopleController
         all_people_from_salesloft.map { |row| row.slice(*PEOPLE_DATA) }
     end
 
+    def emails_characters_frequency_map
+        all_people_from_salesloft.map { |row| row['email_address'] }
+                                 .map { |email| email.split('').group_by(&:itself).transform_values(&:size) }
+                                 .reduce({}) { |acc, hash| acc.merge(hash) { |_, oldval, newval| oldval + newval } }
+                                 .sort_by { |_, v| -v }
+                                 .to_h
+    end
+
     private
 
     def all_people_from_salesloft
-        ::RSalesloft::Resources::People::list["data"]
+        page = 1
+        [].tap do |list|
+            loop do
+                response = ::RSalesloft::Resources::People::list(include_paging_counts: true, page: page)
+                page += 1
+                list.concat(response['data'])
+
+                paging = response['metadata']['paging']
+                break if paging['current_page'] == paging['total_pages']
+            end
+        end
     end
 end
