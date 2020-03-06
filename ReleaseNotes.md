6248238fd66eb9c8ac9bfd5cc40345e04f8d37b6
Ernesto <e.ernesto.espinosa@gmail.com>
2020-03-06 03:05:20 -0600

    [3] Add buttons and templates to show frequency map

--- ReleaseNotes.md
+++ ReleaseNotes.md
@@ -3,3 +3,4 @@
 # Level 1
  - [1] Application scaffolding.
  - [2] Create endpoint to show available People list.
+ - [3] Add a button that shows a frequency map of people's email characters.
--- app/routes/people.rb
+++ app/routes/people.rb
@@ -15,5 +15,12 @@ module SalesLoft
                 @users = SalesLoftController.instance.people_available
             end
         end
+
+        resource :frequency do
+            desc 'Show the frequency map of all characters present in people email addresses'
+            get erb: 'people/frequency' do
+                @frequency_map = []
+            end
+        end
     end
 end
